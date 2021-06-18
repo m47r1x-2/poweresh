bd74b8ad8edc07ba5ba67faa41e8cfa6256e3c02
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-18 08:38:12 +0530

    Add files via upload

---
+++ poweresh.sh
@@ -0,0 +1,42 @@
+#!/bin/bashclear
+
+#########################################################
+clear
+
+apt update && apt upgrade -y
+
+pkg install libcaca
+
+echo -e "\e[92m"
+figlet "It's Me MATRIX"
+
+echo -e "\e[91m"
+figlet "set"
+
+echo -e "\e[32m"
+figlet "aakk"
+
+sleep 1
+
+echo -e "\e[33m"
+figlet "set"
+
+echo -e "\e[34m"
+figlet "aakk"
+
+sleep 1
+
+echo -e "\e[35m"
+figlet "power"
+
+echo -e "\e[36m"
+figlet "varatte"
+
+sleep 1
+
+echo -e "\e[37m"
+figlet "power"
+
+sleep 2
+
+cacafire
